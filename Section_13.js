69c7464ec56adfda579a8dc7304a93ddd960038b
Cloak21 <52053943+Cloak21@users.noreply.github.com>
2019-07-25 11:31:46 -0700

    Commit Section 13

---
+++ Section_13.js
@@ -0,0 +1,9 @@
+document.querySelector("h1") =$("h1")
+$(content)
+$(h1).hasclass(margin 50)
+true
+$(h1).text("Bye")
+$(button).html(em Hey em)
+$(a).attr (href=)
+$("input").keypress(fuction event)
+$(h1).before(button New button)
